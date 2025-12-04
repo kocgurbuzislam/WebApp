9b67395736f18b9f76b3e6dd04bd2df31d8d8b0c
islam Koçgürbüz <islamkocgurbuz9@gmail.com>
2025-12-04 22:29:38 +0300

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        host: '0.0.0.0',
+        port: 4173,
+        allowedHosts: true, // allow Render preview host and others
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
